4c0ed0d8855f31acb6cb46c4af672d2af5a85a03
RoseKeenan <rosemkeenan@gmail.com>
2020-02-23 10:56:08 -0500

    Added lots of stuff

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const axios = require('axios');
+//const axios = require('axios');
 var request = require("request");
 const app = express();
+let sentinel = false;
+
 
 // http://localhost:3000/
 app.get('/', (req, res) => {
@@ -10,63 +12,143 @@ app.get('/', (req, res) => {
     res.send(courseNames);
 });
 
-// /api/courses will be changed to our server
 app.get('/api/courses', (req, res) => { //http://localhost:3000/api/courses
-    const key = 'Bearer JwDTfcunrW6e44vbCZU71-qTTn2JQHCB8U2xfl3xZ6UrPl5VwzPQrg5F_auwixaTdSIe8-FKuPew9_qzbFBjokLyy83EL7GmlKl6aNuEXqJguv1fYCMyoOVGypZRXnYx-qTTn2JQHCB8U2xfl3xZ6UrPl5VwzPQrg5F_auwixaTdSIe8-FKuPew9_qzbFBjokLyy83EL7GmlKl6aNuEXqJguv1fYCMyoOVGypZRXnYx';
-    const term = 'restaurants';
-    const location = 'Chelsea,%New%York';
-    const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`;
-  
-    /*axios({
-        'url': url,
-        'method': 'GET',
-        'headers' : {
-            'Authorization': key
-        }
-    })
-    .then(function(response) {
-        res.send(response)
-        // console.log(someBoolean)
-    })
-    .catch(function (error) {
-        console.log(error);
-    });*/
+    //client stuff
+    const foodOrActivity = '';
+    const rating = '1';
+    const price = '$$';
+    const date = 'MM-DD-YYYY';
+    // const start = '';
+    // const end = '';
+    const day = 2;
+    const term = 'french';
+    var list = [];
 
-    /*axios.create({
-        headers:{
-            Authorization: key,
-            
-        }
-    })
-    .get('https://api.yelp.com/v3/businesses/search?term=restaurants&location=Chelsea,%New%York')
-    .then(resp => res.send(resp))
-    .catch(err => console.log(err));*/
+    const location = 'San%20Francisco,%20CA';
 
-    /*axios.get(url, {
-        headers: {
-          "Authorization": key
-        }
-      })
-    .then(resp => res.send(resp))
-    .catch(err => console.log(err));*/
+    //yelp stuff
+    const key = 'Bearer JwDTfcunrW6e44vbCZU71-qTTn2JQHCB8U2xfl3xZ6UrPl5VwzPQrg5F_auwixaTdSIe8-FKuPew9_qzbFBjokLyy83EL7GmlKl6aNuEXqJguv1fYCMyoOVGypZRXnYx';
+    // const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`;
 
-    var options = { method: 'GET',
+    var options = { 
+        method: 'GET',
         url: 'https://api.yelp.com/v3/businesses/search',
-        qs: { term: 'restaurants', location: 'Chelsea,%25New%25York' },
+        qs: { term: term, location: location },
         headers: { 
-            Authorization: 'Bearer JwDTfcunrW6e44vbCZU71-qTTn2JQHCB8U2xfl3xZ6UrPl5VwzPQrg5F_auwixaTdSIe8-FKuPew9_qzbFBjokLyy83EL7GmlKl6aNuEXqJguv1fYCMyoOVGypZRXnYx' 
+            Authorization: key 
         } 
     };
 
-    request(options, function (error, response, body) {
+    request(options, function(error, response, body) {
         if (error) {
             throw new Error(error)
             console.log('error');
         } else {
             //console.log(response);
-            res.send(body);
+
+            const businessesJS = JSON.parse(body);
+
+            var filteredBusiness = businessesJS.businesses.filter(business => business.categories.filter(category => {
+                if (category.alias === term) {
+                    return true;
+                }
+            }));
+
+            filteredBusiness = filteredBusiness.filter(business => price === business.price);
+
+            filteredBusiness = filteredBusiness.filter(business => rating <= business.rating);
+
+            sentinel = true;
+            
+            var length = filteredBusiness.length;
+
+            var index = Math.floor((Math.random() * length) + 1);
+
+            console.log(index);
+
+            var business = filteredBusiness[index];
+
         };
+
+        const id = business.id;
+
+        var oneOption = { 
+            method: 'GET',
+            url: `https://api.yelp.com/v3/businesses/${id}`,
+            headers: { 
+                Authorization: key 
+            } 
+        };
+
+        request(oneOption, function(error2, response2, body2) {
+            if (error2) {
+                throw new Error(error2)
+                console.log('error');
+            } else {
+                const parsedBody = JSON.parse(body2);
+                var start = parsedBody.hours[0].open;
+
+                start = start.filter(time => day === time.day)
+
+                res.send(start);
+            }
+        })
+
+
+
+        // if (sentinel) {
+
+
+            // for(var i = 0; i <= 10; i++) {
+            //     var business = filteredBusiness[i];
+            //     console.log(business);
+            //     var lessOptions = { 
+            //         method: 'GET',
+            //         url: `https://api.yelp.com/v3/businesses/${business.id}`,
+            //         headers: { 
+            //             Authorization: key 
+            //         } 
+            //     };
+
+                // request(lessOptions, function(error2, response2, body2) {
+                //     if (error2) {
+                //         throw new Error(error2)
+                //         console.log('error');
+                //     } else {
+                //         const parsedBody = JSON.parse(body2);
+                //         list.push(parsedBody);
+                //         console.log(list);
+                //     }
+                // })
+            // }
+
+
+
+            // filteredBusiness.forEach(business => {
+            //         var lessOptions = { 
+            //             method: 'GET',
+            //             url: `https://api.yelp.com/v3/businesses/${business.id}`,
+            //             headers: { 
+            //                 Authorization: key 
+            //             } 
+            //         };
+
+            //         setTimeout(() => {
+            //             request(lessOptions, function(error2, response2, body2) {
+            //                 if (error2) {
+            //                     throw new Error(error2)
+            //                     console.log('error');
+            //                 } else {
+            //                     const parsedBody = JSON.parse(body2);
+            //                     list.push(parsedBody);
+            //                     console.log(list);
+            //                 }
+            //             })
+            //         }, 3000);          
+            // })
+        // };     
     });
+
 })
 
 const port = process.env.PORT || 3000;
