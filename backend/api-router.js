b61675944b4fbee9cdfd1ccc8880f3a60b7e0bb8
Wilson Diaz <wdfiguereo@gmail.com>
2020-08-02 12:20:57 -0400

    remove unused functions

--- backend/api-router.js
+++ backend/api-router.js
@@ -7,14 +7,5 @@ const activityRouter = require('./activity.js');
 router.use(express.json());
 router.use('/food', foodRouter);
 router.use('/activity', activityRouter);
-router.use(function(req, res, next) {
-
-  res.header("Access-Control-Allow-Origin", "*");
-
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  next();
-
-});
 
 module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./api-router.js');
 app.use('/api', apiRouter);
-app.use(function(req, res, next) {
-  next();
-});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on post ${port}....`));
