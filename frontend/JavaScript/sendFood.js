57143afb850fe44c1d8eb03bf450d20b87cafa7b
Wilson Diaz <wdfiguereo@gmail.com>
2020-08-02 11:37:58 -0400

    catch promise errors

--- frontend/JavaScript/sendFood.js
+++ frontend/JavaScript/sendFood.js
@@ -2,7 +2,8 @@
 // will need to: send user input, receive back-end response, 
 // route user to landing page, somehow get the data to that page to be displayed
 async function sendFood(data) {
-    let resp = await postUserData('http://localhost:3000/api/food', data);
+    let resp = await postUserData('http://localhost:3000/api/food', data)
+    .catch(e => console.log(e));
     console.log(resp);
     // return resp?
 }
