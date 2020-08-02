3bf66f74f0220f3bea300465a0f541d6fb41bca1
Wilson Diaz <wdfiguereo@gmail.com>
2020-08-02 00:33:08 -0400

    change food endpoint from GET to POST

--- backend/food.js
+++ backend/food.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 var request = require("request");
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     //client stuff
     var rating = '1';
     var price = '$$';
