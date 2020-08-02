f6c6a7c2fa29877b7899b7a1e8ceda09e32fbcdc
Wilson Diaz <wdfiguereo@gmail.com>
2020-08-02 11:34:26 -0400

    remove CORS headers

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,7 @@ const app = express();
 const apiRouter = require('./api-router.js');
 app.use('/api', apiRouter);
 app.use(function(req, res, next) {
-
-  res.header("Access-Control-Allow-Origin", "*");
-
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
   next();
-
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on post ${port}....`));
