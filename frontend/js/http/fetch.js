57e64490eb66ee3e32abcdaf7bb756ac4167e331
Wilson Diaz <wdfiguereo@gmail.com>
2020-08-02 02:10:35 -0400

    add "sending" message to the post method

--- frontend/js/http/fetch.js
+++ frontend/js/http/fetch.js
@@ -1,5 +1,6 @@
 // sends user input options to specified url and awaits a response (date location)
 async function postUserData(url, data) {
+  console.log('sending...');
   const response = await fetch(url, {
     method: 'POST',
     body: JSON.stringify(data)
