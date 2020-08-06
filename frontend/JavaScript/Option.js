37fcdd8d653b6eedca946406f9c1a9aa39368c70
Wilson Diaz <wdfiguereo@gmail.com>
2020-08-05 20:17:49 -0400

    correct button sends food request

--- frontend/JavaScript/Option.js
+++ frontend/JavaScript/Option.js
@@ -13,7 +13,7 @@ function ActivityOption() {
             <span style="width:35%" onclick="btnClick(this, 'btnYes')" class="btn  btn-lg btn-secondary btn-option" id="btnNo">No, submit what I have!</a>
         </p>
         <p class="lead">
-            <span id="button" onclick="sendFood(answers)" class="btn btn-lg btn-secondary">Next</span>
+            <span id="button" class="btn btn-lg btn-secondary">Next</span>
         </p>
 `;
     console.log(answers);
--- frontend/JavaScript/sendFood.js
+++
@@ -1,9 +0,0 @@
-// spaghetti for now, I just want to get the http req working
-// will need to: send user input, receive back-end response, 
-// route user to landing page, somehow get the data to that page to be displayed
-async function sendFood(data) {
-    let resp = await postUserData('http://localhost:3000/api/food', data)
-    .catch(e => console.log(e));
-    console.log(resp);
-    // return resp?
-}
